ddfcacd66ef04e23acd5967ea68a9c67fdba4ee9
Frank Hu <tianyaoh@hotmail.com>
2024-01-04 05:18:18 +0000

    launch file: start_gazebo_world; and spawn 1barista robot

--- launch/barista_urdf.launch.py
+++ launch/barista_urdf.launch.py
@@ -1,13 +1,20 @@
 import os
+import random
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import Command
 from launch_ros.actions import Node
 
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_prefix
+
 # this is the function launch  system will look for
 def generate_launch_description():
 
+    ############### RVIZ ################
     ####### DATA INPUT ##########
     urdf_file = 'barista_robot_model.urdf'
     #xacro_file = "box_bot.xacro"
@@ -40,9 +47,80 @@ def generate_launch_description():
             parameters=[{'use_sim_time': True}],
             arguments=['-d', rviz_config_dir])
 
+
+    ###################__Gazebo__####################
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_box_bot_gazebo = get_package_share_directory('barista_robot_description')
+
+    # We get the whole install dir
+    # We do this to avoid having to copy or softlink manually the packages so that gazebo can find them
+    description_package_name = "barista_robot_description"
+    install_dir = get_package_prefix(description_package_name)
+
+    # Set the path to the WORLD model files. Is to find the models inside the models folder in my_box_bot_gazebo package
+    # gazebo_models_path = os.path.join(pkg_box_bot_gazebo, 'models')
+    # # os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+
+    # if 'GAZEBO_MODEL_PATH' in os.environ:
+    #     os.environ['GAZEBO_MODEL_PATH'] =  os.environ['GAZEBO_MODEL_PATH'] + ':' + install_dir + '/share' + ':' + gazebo_models_path
+    # else:
+    #     os.environ['GAZEBO_MODEL_PATH'] =  install_dir + "/share" + ':' + gazebo_models_path
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    
+
+    # print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
+
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
+    )
+
+    ############__SPAWN_ROBOT__####################
+    # Position and orientation
+    # [X, Y, Z]
+    position = [0.0, 0.0, 0.2]
+    # [Roll, Pitch, Yaw]
+    orientation = [0.0, 0.0, 0.0]
+    # Base Name or robot
+    robot_base_name = "barista_robot"
+
+
+    entity_name = robot_base_name+"-"+str(int(random.random()*100000))
+
+    # Spawn ROBOT Set Gazebo
+    spawn_robot = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        name='spawn_entity',
+        output='screen',
+        arguments=['-entity',
+                   entity_name,
+                   '-x', str(position[0]), '-y', str(position[1]
+                                                     ), '-z', str(position[2]),
+                   '-R', str(orientation[0]), '-P', str(orientation[1]
+                                                        ), '-Y', str(orientation[2]),
+                   '-topic', '/robot_description'
+                   ]
+    )
+
+
     # create and return launch description object
     return LaunchDescription(
         [            
+            DeclareLaunchArgument(
+          'world',
+          default_value=[os.path.join(pkg_box_bot_gazebo, 'worlds', 'empty.world'), ''],
+          description='SDF world file'),
+            gazebo,
+            spawn_robot,
             robot_state_publisher_node,
             rviz_node
         ]
