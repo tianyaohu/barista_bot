27594f19e00df947fce59a4cba3304e70fd89169
Frank Hu <tianyaoh@hotmail.com>
2024-01-04 18:22:22 +0000

    xacrofy: base_link and chassis

---
+++ launch/barista_xacro.launch.py
@@ -0,0 +1,94 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_prefix
+
+import xacro
+
+
+def generate_launch_description():
+
+    ###################__Gazebo__####################
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_barista_bot_gazebo = get_package_share_directory('barista_robot_description')
+
+    # We get the whole install dir
+    # We do this to avoid having to copy or softlink manually the packages so that gazebo can find them
+    description_package_name = "barista_robot_description"
+    install_dir = get_package_prefix(description_package_name)
+
+    # Set the path to the WORLD model files. Is to find the models inside the models folder in my_box_bot_gazebo package
+    gazebo_models_path = os.path.join(pkg_barista_bot_gazebo, 'meshes')
+    # os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] =  os.environ['GAZEBO_MODEL_PATH'] + ':' + install_dir + '/share' + ':' + gazebo_models_path
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] =  install_dir + "/share" + ':' + gazebo_models_path
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'))
+    )
+
+    robot_model_path = os.path.join(
+        get_package_share_directory('barista_robot_description'))
+
+    xacro_file = os.path.join(robot_model_path, 'xacro', 'barista_robot_model.urdf.xacro')
+
+    print("xacro_file", xacro_file)
+
+    # convert XACRO file into URDF
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    params = {'robot_description': doc.toxml()}
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-entity', 'barista_bot', '-x', '1.0', '-y', '1.0', '-z', '0.2',
+                                   '-topic', 'robot_description'],
+                        output='screen')
+
+    ############### RVIZ ################
+    ####### DATA INPUT ##########
+    # urdf_file = 'barista_robot_model.urdf'
+    xacro_file = "barista_robot_model.xacro"
+    package_description = "barista_robot_description"
+
+
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'xacro_vis.rviz')
+
+
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir])
+
+    return LaunchDescription([
+        gazebo,
+        spawn_entity,
+        rviz_node,
+        robot_state_publisher,
+    ])
